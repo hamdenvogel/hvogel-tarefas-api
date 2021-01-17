f7ed24d36c9f14cca66edab96c9cd35ccee983ab
Pedra <F796071@mail.caixa>
2021-01-17 18:02:50 -0300

    first commit

---
+++ src/main/java/io/github/dougllas/todoapi/model/entity/Todo.java
@@ -0,0 +1,33 @@
+package io.github.dougllas.todoapi.model.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.github.dougllas.todoapi.model.enums.Categoria;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "todo" , schema = "tarefas")
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String descricao;
+
+    private boolean done;
+
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+
+}
